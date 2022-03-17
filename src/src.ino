80392ef5d665b8b827826238193b08515d66ab11
victortaksheyev <victortaksheyev@gmail.com>
2022-03-17 12:58:16 -0700

    aquire data from IMU

--- src/src.ino
+++ src/src.ino
@@ -1,15 +1,131 @@
 // main setup and loop for GLDRR
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+uint8_t sysCal, gyroCal, accelCal, magCal = 0; // calibration flags
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
+  Serial.begin(115200);
+  //BNO055
+  if (!bno.begin())
+  {
+    Serial.print("no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  /* begin - aquire IMU (BNO055 data) */ 
+  sensors_event_t angVelocityData, accelerometerData, gravityData;
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+ if (imuCalibrated()) {
+    printEvent(&angVelocityData);
+    printEvent(&accelerometerData);
+    printEvent(&gravityData);
+  } else {
+   printIMUCalibration();
+  }
+  
+  /* end - aquire IMU (BNO055 data) */ 
+
+ 
+
+  
+
+  /*
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
+  delay(100);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  delay(100);                       // wait for a second
+  */
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+}
+
+void printEvent(sensors_event_t* event) {
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+    Serial.print("Accl:\t");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+    Serial.print("Orient:\t");
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+    Serial.print("Mag:\t");
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+  }
+  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+    Serial.print("Gyro:\t");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+    Serial.print("Rot:\t");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+    Serial.print("Linear:\t");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_GRAVITY) {
+    Serial.print("Gravity:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else {
+    Serial.print("Unk:\t");
+  }
+
+  Serial.print("\tx= ");
+  Serial.print(x);
+  Serial.print(" |\ty= ");
+  Serial.print(y);
+  Serial.print(" |\tz= ");
+  Serial.println(z);
+}
+
+/* Returns true if IMU (BNO055) is calibrated - false otherwise */
+bool imuCalibrated() {
+  bno.getCalibration(&sysCal, &gyroCal, &accelCal, &magCal);
+  if (gyroCal == 3 && accelCal == 3 && magCal == 3)
+    return true;
+   return false;
+}
+
+void printIMUCalibration() {
+//  bno.getCalibration(&sysCal, &gyroCal, &accelCal, &magCal);
+  Serial.println();
+  Serial.print("Calibration: Sys=");
+  Serial.print(sysCal);
+  Serial.print(" Gyro=");
+  Serial.print(gyroCal);
+  Serial.print(" Accel=");
+  Serial.print(accelCal);
+  Serial.print(" Mag=");
+  Serial.println(magCal);
 }
